465e49eb488d3257dc382dbe880c048deedf6ea0
vdhulappanavar <vdhulappanavar@gmail.com>
2024-09-21 16:11:52 +0530

    first commit

--- next.config.mjs
+++ next.config.mjs
@@ -14,7 +14,7 @@ export default withSentryConfig(
     // For all available options, see:
     // https://github.com/getsentry/sentry-webpack-plugin#options
 
-    org: "javascript-mastery",
+    org: "student-hl4",
     project: "care-pulse",
 
     // Only print logs for uploading source maps in CI
